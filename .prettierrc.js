22575e155835a55957257f540bae9930a70bacdd
SHØ <neila@users.noreply.github.com>
2023-04-11 14:23:00 +0900

    apply prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "trailingComma": "all",
-  "tabWidth": 2,
-  "singleQuote": true,
-  "semi": true,
-  "importOrder": ["^[./]", "^@/(.*)$"],
-  "importOrderSeparation": true,
-  "importOrderSortSpecifiers": true,
-  "importOrderGroupNamespaceSpecifiers": true,
-  "importOrderCaseInsensitive": true,
-  "plugins": ["@trivago/prettier-plugin-sort-imports"]
-}
+  trailingComma: 'all',
+  tabWidth: 2,
+  singleQuote: true,
+  semi: true,
+  importOrder: ['^[./]', '^@/(.*)$'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderGroupNamespaceSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+};
