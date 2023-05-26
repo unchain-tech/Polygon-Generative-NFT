a52fa8033e165457a9e9b41a8d6cde01056ace17
github honganji <yujicavs@icloud.com>
2023-05-26 14:31:20 +0900

    made client derectory

---
+++ packages/client/src/App.js
@@ -0,0 +1,109 @@
+import { useEffect, useState } from 'react';
+
+import './App.css';
+import contract from './contracts/NFTCollectible.json';
+
+const contractAddress = '0xFD1035BFf7F4825c99c8cBC6fDAACDdca21DC3Ec';
+const abi = contract.abi;
+const ethers = require('ethers');
+
+function App() {
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log('Make sure you have MetaMask installed!');
+      return;
+    } else {
+      console.log("Wallet exists! We're ready to go!");
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log('Found an authorized account: ', account);
+      setCurrentAccount(account);
+    } else {
+      console.log('No authorized account found');
+    }
+  };
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert('Please install MetaMask!');
+    }
+
+    try {
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      console.log('Found an account! Address: ', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const mintNftHandler = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+        console.log('Initialize payment');
+        let nftTxn = await nftContract.mintNFTs(1, {
+          value: ethers.utils.parseEther('0.01'),
+        });
+
+        console.log('Mining... please wait');
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: ${nftTxn.hash}`);
+      } else {
+        console.log('Ethereum object does not exist');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const connectWalletButton = () => {
+    return (
+      <button
+        onClick={connectWalletHandler}
+        className="cta-button connect-wallet-button"
+      >
+        Connect Wallet
+      </button>
+    );
+  };
+
+  const mintNftButton = () => {
+    return (
+      <button onClick={mintNftHandler} className="cta-button mint-nft-button">
+        Mint NFT
+      </button>
+    );
+  };
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, []);
+
+  return (
+    <div className="main-app">
+      <h1>Scrappy Squirrels Tutorial</h1>
+      <div>{currentAccount ? mintNftButton() : connectWalletButton()}</div>
+    </div>
+  );
+}
+
+export default App;
